f7a1b6107bc53c11aa15aeb4d02883e238e91a58
palloncino <antonio.guiotto.dev@gmail.com>
2022-04-24 14:36:12 +0200

    renewed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-const {Builder} = require('selenium-webdriver');
+/* eslint-disable no-constant-condition */
+/* eslint-disable no-await-in-loop */
+const {Builder, By, until} = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
 async function init() {
@@ -9,11 +11,36 @@ async function init() {
 			.setChromeOptions(options)
 			.build();
 
-		await driver.quit();
+		// Go to badoo
+		await driver.get('https://badoo.com');
+
+		// 🚨 click accept cookie manually
+
+		// 🚨 click signin cookie manually
+
+		// 🚨 insert email and password manually and proceed with verification
+
+		const likeButtonRef = By.xpath('//*[@id="mm_cc"]/div[1]/section/div/div[2]/div/div[2]/div[1]/div[1]');
+		await driver.wait(until.elementLocated(likeButtonRef), 60000);
+		const likeButton = driver.findElement(likeButtonRef);
+		likeButton.click();
+
+		const wannaBeNotifiedRef = By.xpath('/html/body/aside/section/div[1]/div/div/section/div/div/div/div[2]/div');
+		await driver.wait(until.elementLocated(wannaBeNotifiedRef), 60000);
+		const wannaBeNotifiedButton = driver.findElement(wannaBeNotifiedRef);
+		wannaBeNotifiedButton.click();
+
+		while (true) {
+			await driver.actions().pause(500).perform();
+			const likeButtonRef = By.xpath('//*[@id="mm_cc"]/div[1]/section/div/div[2]/div/div[2]/div[1]/div[1]');
+			await driver.wait(until.elementLocated(likeButtonRef), 10000);
+			const likeButton = await driver.findElement(likeButtonRef);
+			driver.wait(until.elementIsVisible(likeButton), 10000);
+			likeButton.click();
+		}
 	} catch (error) {
 		console.log(error);
 	}
 }
 
 init();
-
