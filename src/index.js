4f45270a8eafb3c629220d93f08a9355b5f05a38
palloncino <antonio.guiotto.dev@gmail.com>
2022-04-24 12:19:14 +0200

    fresh start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('Hello');
+const {Builder} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+
+async function init() {
+	try {
+		const options = new chrome.Options();
+		const driver = await new Builder()
+			.forBrowser('chrome')
+			.setChromeOptions(options)
+			.build();
+
+		await driver.quit();
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+init();
+
